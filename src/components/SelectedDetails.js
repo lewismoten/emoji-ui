bd61e88e155bff1130040829c64cfce56dae2610
Lewis Moten <lewismoten@gmail.com>
2021-11-27 16:37:17 -0500

    show title as drawer title, thus adding close button

--- src/components/SelectedDetails.js
+++ src/components/SelectedDetails.js
@@ -4,12 +4,15 @@ import {
   PanelGroup,
   Drawer
 } from 'rsuite'
+import CloseOutlineIcon from '@rsuite/icons/CloseOutline'
 
 const SelectedDetails = ({ isVisible, id, text, onClose, title }) => {
   return (
     <Drawer open={isVisible} onClose={onClose} size="xs" backdrop={false}>
       <PanelGroup>
-        <Panel>{title}</Panel>
+        <Drawer.Header>
+          <Drawer.Title>{title}</Drawer.Title>
+        </Drawer.Header>
         <Panel>{id}</Panel>
         <Panel className="oversized-text">{text}</Panel>
       </PanelGroup>
