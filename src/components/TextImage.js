e4a90e5d6090404db089fa3b8060b099f41f880b
Lewis Moten <lewismoten@gmail.com>
2021-11-27 21:52:55 -0500

    restrict max width without squeezing

--- src/components/TextImage.js
+++ src/components/TextImage.js
@@ -1,9 +1,14 @@
-import React, { useRef, useEffect } from 'react';
+import React, { useRef, useEffect, useState, useCallback } from 'react';
 import PropTypes from 'prop-types';
+
+const GROW = 3;
+const SHRINK = 1;
 
 const TextImage = ({ text }) => {
   const canvasRef = useRef(null);
   const frameRequest = useRef();
+
+  const [fontSize, setFontSize] = useState(128);
 
   const onFrame = () => {
     if (!canvasRef.current) return;
@@ -13,26 +18,39 @@ const TextImage = ({ text }) => {
     canvas.height = height;
     const ctx = canvas.getContext('2d');
 
-    console.log({ width, height });
-
-    ctx.font = `${height / 1.5}px sans-serif`;
     ctx.fillStyle = 'rgb(0, 0, 0)';
     ctx.textAlign = 'center';
     ctx.textBaseline = 'middle';
-    ctx.fillText(text, width / 2, height / 2, width);
+    ctx.font = `${fontSize}px sans-serif`;
+
+    const size = ctx.measureText(text);
+    const isTooBig = size.width > width;
+    const isTooSmall = size.width + GROW < width;
+    if (isTooBig) {
+      setFontSize(fontSize - SHRINK);
+    } else if (isTooSmall) {
+      setFontSize(fontSize + GROW);
+    }
+
+    ctx.fillText(text, width / 2, height / 2);
     frameRequest.current = requestAnimationFrame(onFrame);
   };
 
+  const onFrameCallback = useCallback(onFrame, [fontSize, onFrame]);
   useEffect(() => {
-    frameRequest.current = requestAnimationFrame(onFrame);
+    frameRequest.current = requestAnimationFrame(onFrameCallback);
     return () => {
       cancelAnimationFrame(frameRequest.current);
     };
   }, [onFrame]);
 
+  useEffect(() => {
+    console.log(text, fontSize);
+  }, [fontSize, text]);
+
   return (
     <div style={{ minWidth: '256px', minHeight: '256px' }}>
-      <canvas ref={canvasRef} width="256" height="256" />
+      <canvas ref={canvasRef} width="256" height="300" />
     </div>
   );
 };
