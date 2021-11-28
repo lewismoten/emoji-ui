b850f5ef2e1a1dcb010b4410194b538d27ba8db5
Lewis Moten <lewismoten@gmail.com>
2021-11-27 19:18:09 -0500

    change grid to list

--- src/components/DetailView.js
+++ src/components/DetailView.js
@@ -1,24 +1,41 @@
 import React from 'react'
-import { Grid, Row, Col } from 'rsuite'
+import {
+  List,
+  FlexboxGrid
+} from 'rsuite'
 import CodePoints from './CodePoints'
 import EmojiButton from './EmojiButton'
 
 const DetailView = ({ list, size, onSelect, selectedId }) => (
-  <Grid>
+  <List hover>
     {list.map(({ key, value, text, encodedValue, codePoints }) => {
       const onClick = () => onSelect(key);
       const isSelected = selectedId === key;
+
       return (
-      <Row key={key} onClick={onClick}>
-        <Col xs={1}>
-          <EmojiButton value={value} size={size} onClick={onClick} isSelected={isSelected} />
-        </Col>
-        <Col xs={6}><span className='object'>emoji<span className='delimiter'>.</span><span className='key'>{key}</span></span></Col>
-        <Col xs={9}><CodePoints value={codePoints} format="es6" /></Col>
-        <Col xs={8}><span className='comment'>{text}</span></Col>
-      </Row>
+        <List.Item key={key} onClick={onClick}>
+          <FlexboxGrid>
+            <FlexboxGrid.Item colspan={1}>
+              <EmojiButton
+                value={value}
+                size={size}
+                onClick={onClick}
+                isSelected={isSelected}
+              />
+            </FlexboxGrid.Item>
+            <FlexboxGrid.Item colspan={6}>
+              <span className='object'>emoji<span className='delimiter'>.</span><span className='key'>{key}</span></span>
+            </FlexboxGrid.Item>
+            <FlexboxGrid.Item colspan={9}>
+              <CodePoints value={codePoints} format="es6" />
+            </FlexboxGrid.Item>
+            <FlexboxGrid.Item colspan={8}>
+              <span className='comment'>{text}</span>
+            </FlexboxGrid.Item>
+          </FlexboxGrid>
+        </List.Item>
     )})}
-  </Grid>
+  </List>
 )
 
 export default DetailView
